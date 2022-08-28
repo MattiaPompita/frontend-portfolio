af7d74d21ac21712fc5948fd275e84c8bc309045
MattiaPompita <mattiapompita@gmail.com>
2022-08-28 15:16:24 +0200

    new static hero

--- src/app/animations/hero.js
+++ src/app/animations/hero.js
@@ -2,37 +2,5 @@ import anime from "animejs/lib/anime.es.js";
 
 
 export class HeroAnim {
-  heroTextAnim = anime
-    .timeline({ autoplay: false })
-    .add({
-      targets: [".hero-section__main__presentation__helloMsg"],
-      translateY: [200, 0],
-      opacity: [0, 1],
-      easing: "easeOutSine",
-      duration: 1000,
-    })
-    .add({
-      targets: [
-        ".hero-section__main__presentation__p",
-        ".navbar-brand",
-        ".navbar__container__link",
-      ],
-      delay: anime.stagger(200),
-      translateY: [100, 0],
-      opacity: [0, 1],
-      easing: "easeOutSine",
-      duration: 1000,
-      begin: function () {
-        let heroIconPath = anime.path("#icon-hero-path path");
-
-        return anime.timeline({ loop: true }).add({
-          targets: ".hero-section__main__presentation__icon",
-          translateY: heroIconPath("y"),
-          opacity: [0.3, 0.8, 1, 0.8, 0.3],
-          duration: 2000,
-          easing: "easeInOutSine",
-          direction: "reverse",
-        });
-      },
-    });
+ 
 }
--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,6 @@
 require("waypoints/lib/noframework.waypoints.min");
 
-const aboutMeContainer = document.querySelector(".about-me");
-const contextWrapper = document.querySelector(".wrapper");
 
-export const run = (Navbar, Hero, AboutMe) => {
-  Navbar.renderAnim();
-  Hero.heroTextAnim.play();
-
-  var waypoint = new Waypoint({
-    element: aboutMeContainer,
-    handler: function () {
-      AboutMe.aboutMeTitle(Waypoint.viewportHeight());
-      waypoint.disable();
-    },
-    context: contextWrapper,
-  });
+export const run = ( Hero, AboutMe) => {
+  
 };
